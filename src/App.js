c89648192a1f64f65fbf923b4214262170c6d9e3
aayushGarg-1748 <argtech.1234@gmail.com>
2020-12-01 16:59:37 +0530

    some updates

--- src/App.js
+++ src/App.js
@@ -1,30 +1,25 @@
 import React from 'react'
 import './App.css';
 import NavigationBar from './Component/NavigationBar';
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './Component/pages/Home'
 import GithubRepositories from './Component/pages/GithubRepositories'
 import CyborticsHumanoids from './Component/pages/CyborticsHumanoid'
 import AboutMe from './Component/pages/AboutMe'
-
+import Aihome from "./Component/Aihome";
 
 function App() {
   return (
     <>
       <Router>
-        <NavigationBar />
+        <NavigationBar/>
         <Switch>
           <Route path={process.env.PUBLIC_URL + '/'} exact component={Home} />
-          <Route path={process.env.PUBLIC_URL + '/about-me'} exact component={AboutMe}/>
-          <Route path={process.env.PUBLIC_URL + '/github-repos'} exact component={GithubRepositories}/>
-          <Route path={process.env.PUBLIC_URL + '/cybortics-humanoid'} exact component={CyborticsHumanoids}/>
-          <Route path={process.env.PUBLIC_URL + '/contact-me'}/>
-          {/* <Route path='' exact component={}/> */}
-          {/* <Route path='' exact component={}/> */}
-          {/* <Route path='' exact component={}/> */}
-          {/* <Route path='' exact component={}/> */}
-          {/* <Route path='' exact component={}/> */}
-          {/* <Route path='' exact component={}/> */}
+          <Route path={process.env.PUBLIC_URL + '/about-me'} exact component={AboutMe} />
+          <Route path={process.env.PUBLIC_URL + '/github-repos'} exact component={GithubRepositories} />
+          <Route path={process.env.PUBLIC_URL + '/cybortics-humanoid'} exact component={CyborticsHumanoids} />
+          <Route path={process.env.PUBLIC_URL + '/contact-me'} />
+          <Route path={process.env.PUBLIC_URL + '/artificialTechnologyHomework'} exact component={Aihome}/>
         </Switch>
       </Router>
     </>
