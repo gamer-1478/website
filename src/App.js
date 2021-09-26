ce81c7051f29b9a33146aa4d5d12e2faa517c794
gamer-12748 <aayushgarg2006@gmail.com>
2021-09-26 15:31:44 +0530

    workflow

--- src/App.js
+++ src/App.js
@@ -13,9 +13,9 @@ import QuizLoad from './Component/pages/quiz/QuizLoad';
 
 function Aihome() {
   return (
-      <div>
+    <div>
       {window.location.href = "https://sway.office.com/92QqdnEbHCOkIfjA?ref=Link"}
-      </div>
+    </div>
   )
 }
 function App() {
@@ -23,18 +23,19 @@ function App() {
     <div>
       <Router>
         <NavigationBar />
-        <Switch>
-          <Route path={process.env.PUBLIC_URL + '/'} exact component={Home} />
-          <Route path={process.env.PUBLIC_URL + '/about-me'} exact component={AboutMe} />
-          <Route path={process.env.PUBLIC_URL + '/github-repos'} exact component={GithubRepositories} />
-          <Route path={process.env.PUBLIC_URL + '/cybortics-humanoid'} exact component={CyborticsHumanoids} />
-          <Route path={process.env.PUBLIC_URL + '/ai'} exact component={Aihome} />
-          {/* <Route path={process.env.PUBLIC_URL + '/testimonials'} exact component={Testimonials } /> */}
-          {/* <Route path={process.env.PUBLIC_URL + '/ai-dsm'} exact component={Dsm} /> */}
-          <Route path={process.env.PUBLIC_URL + '/quiz-science-hhw'} exact component={QuizLoad}/>
-          <Route path="*" exact component={fourzerofour} />
-        </Switch>
-        <Footer/>
+        <div style={{minHeight:'78.1vh'}}>
+          <Switch>
+            <Route path={process.env.PUBLIC_URL + '/'} exact component={Home} />
+            <Route path={process.env.PUBLIC_URL + '/about-me'} exact component={AboutMe} />
+            <Route path={process.env.PUBLIC_URL + '/github-repos'} exact component={GithubRepositories} />
+            <Route path={process.env.PUBLIC_URL + '/cybortics-humanoid'} exact component={CyborticsHumanoids} />
+            <Route path={process.env.PUBLIC_URL + '/ai'} exact component={Aihome} />
+            {/* <Route path={process.env.PUBLIC_URL + '/ai-dsm'} exact component={Dsm} /> */}
+            <Route path={process.env.PUBLIC_URL + '/quiz-science-hhw'} exact component={QuizLoad} />
+            <Route path="*" exact component={fourzerofour} />
+          </Switch>
+        </div>
+        <Footer />
       </Router>
     </div>
   );
