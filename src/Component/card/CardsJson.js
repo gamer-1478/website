cb2d0e73b327fb0bcb33f5af591c5d0c3a375ccd
gamer-12748 <aayushgarg2006@gmail.com>
2021-09-27 17:28:10 +0530

    update

--- src/Component/card/CardsJson.js
+++ src/Component/card/CardsJson.js
@@ -1,6 +1,20 @@
 export default function CardJSON() {
     return JSON.parse(JSON.stringify([
         {
+            img_src: 'https://socialify.git.ci/aayushGarg-1748/open-mrs-mail/image?description=1&font=Raleway&issues=1&language=1&pattern=Charlie%20Brown&pulls=1&stargazers=1&theme=Dark',
+            text: 'GCI OpenMRS (2019).',
+            source: 'https://github.com/aayushGarg-1748/open-mrs-mail',
+            label: 'HTML CSS',
+            deploy: 'https://aayushgarg-1748.github.io/open-mrs-mail/'
+        },
+        {
+            img_src: 'https://socialify.git.ci/aayushGarg-1748/openmrs-404/image?description=1&font=Raleway&issues=1&language=1&pattern=Charlie%20Brown&pulls=1&stargazers=1&theme=Dark',
+            text: 'GCI OpenMRS 404 Page Task (2019).',
+            source: 'https://github.com/aayushGarg-1748/openmrs-404',
+            label: 'HTML CSS',
+            deploy: 'https://aayushgarg-1748.github.io/openmrs-404/'
+        },
+        {
             img_src: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/Simple_Periodic_Table_Chart-blocks.svg/1200px-Simple_Periodic_Table_Chart-blocks.svg.png",
             text: 'MCQ Periodic Table Quiz, Randomly generated 10 questions.',
             label: 'Chemistry-Quiz',
@@ -26,6 +40,13 @@ export default function CardJSON() {
             deploy: '/ai-dsm',
             source: 'https://github.com/gamer-1478/ai-dsm'
 
+        },
+        {
+            img_src:'https://socialify.git.ci/gamer-1478/toycathon/image?description=1&font=Raleway&issues=1&language=1&pattern=Charlie%20Brown&pulls=1&stargazers=1&theme=Dark',
+            text: 'Toycathon is a GOV hosted Comp, for which we made a android app with a physical board game and won.',
+            source: 'https://github.com/gamer-1478/toycathon',
+            label:'Android Game Flutter',
+            deploy: 'https://google.com',
         }
     ]))
 }
