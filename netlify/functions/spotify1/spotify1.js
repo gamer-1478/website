18220fcba41b1a9aa070cda62abd2b3b2c2f0b4c
gamer-1478 <aayushgarg2006@gmail.com>
2021-09-28 13:59:15 +0530

    update

--- netlify/functions/spotify1/spotify1.js
+++ netlify/functions/spotify1/spotify1.js
@@ -8,7 +8,7 @@ exports.handler = async function () {
     try {
       await fetch(API_ENDPOINT, {
         "method": "GET",
-        "Authorization": "Bearer BQD7zLtFuHVpB0A71Pmcme4DC9u3gGEESInayOD4dyk_hAa7nOr0o79FfPka3ru0HGgog4comIvcrg5-SocDhJeVuKo_RrabGeJvvLuUz0nARTL_sY_pCIzQCg5AQ1_azpgbJAXqtCWm7_ZtURKYx8X3ScR3dkYu1hFI",
+        "headers": { "Authorization": "Bearer BQD7zLtFuHVpB0A71Pmcme4DC9u3gGEESInayOD4dyk_hAa7nOr0o79FfPka3ru0HGgog4comIvcrg5-SocDhJeVuKo_RrabGeJvvLuUz0nARTL_sY_pCIzQCg5AQ1_azpgbJAXqtCWm7_ZtURKYx8X3ScR3dkYu1hFI" }
       }).then((result) => { resolve(result.json()) })
       // handle response
     } catch (err) {
@@ -20,7 +20,7 @@ exports.handler = async function () {
       })
     }
   }).then((resolve) => { return resolve })
-  .catch((reject) => { return reject })
+    .catch((reject) => { return reject })
 
   return {
     statusCode: 200,
