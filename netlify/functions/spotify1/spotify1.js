a2c58b08be6afc46c7b8e8acb424651fe1bc038a
gamer-1478 <aayushgarg2006@gmail.com>
2021-09-28 13:44:41 +0530

    update

---
+++ netlify/functions/spotify1/spotify1.js
@@ -0,0 +1,31 @@
+// for a full working demo of Netlify Identity + Functions, see https://netlify-gotrue-in-react.netlify.com/
+
+const fetch = require('node-fetch')
+const API_ENDPOINT = 'https://api.spotify.com/v1/me/player/currently-playing?market=IN'
+
+const handler = async function (event, context) {
+  let response
+  try {
+    response = await fetch(API_ENDPOINT, {
+      "method": "GET",
+      "Authorization": "Bearer BQD7zLtFuHVpB0A71Pmcme4DC9u3gGEESInayOD4dyk_hAa7nOr0o79FfPka3ru0HGgog4comIvcrg5-SocDhJeVuKo_RrabGeJvvLuUz0nARTL_sY_pCIzQCg5AQ1_azpgbJAXqtCWm7_ZtURKYx8X3ScR3dkYu1hFI",
+      "Content-Type": "application/json"
+    })
+    // handle response
+  } catch (err) {
+    return {
+      statusCode: err.statusCode || 500,
+      body: JSON.stringify({
+        error: err.message
+      })
+    }
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      data: response
+    })
+  }}
+
+module.exports = { handler }
