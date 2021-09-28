7681be1647ea1dddd6fbf0fc511bfb085ea87836
gamer-1478 <aayushgarg2006@gmail.com>
2021-09-28 13:56:27 +0530

    hmm

--- netlify/functions/spotify1/spotify1.js
+++ netlify/functions/spotify1/spotify1.js
@@ -9,7 +9,6 @@ exports.handler = async function () {
       await fetch(API_ENDPOINT, {
         "method": "GET",
         "Authorization": "Bearer BQD7zLtFuHVpB0A71Pmcme4DC9u3gGEESInayOD4dyk_hAa7nOr0o79FfPka3ru0HGgog4comIvcrg5-SocDhJeVuKo_RrabGeJvvLuUz0nARTL_sY_pCIzQCg5AQ1_azpgbJAXqtCWm7_ZtURKYx8X3ScR3dkYu1hFI",
-        "Content-Type": "application/json"
       }).then((result) => { resolve(result.json()) })
       // handle response
     } catch (err) {
