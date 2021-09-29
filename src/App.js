8d7c97903735848a4b12dca12554fcf9924609ab
gamer-1478 <aayushgarg2006@gmail.com>
2021-09-29 09:53:12 +0530

    remove

--- src/App.js
+++ src/App.js
@@ -3,12 +3,9 @@ import './App.css';
 import NavigationBar from './Component/navigation/NavigationBar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './Component/pages/Home'
-import GithubRepositories from './Component/pages/gitrepos/GithubRepositories'
 import CyborticsHumanoids from './Component/pages/cybortics/CyborticsHumanoid'
 import Footer from './Component/footer/Footer'
 import fourzerofour from "./Component/404/404";
-import Test from './Component/test'
-//import QuizLoad from './Component/pages/quiz/QuizLoad';
 
 function Aihome() {
   return (
@@ -34,12 +31,9 @@ function App() {
         <div style={{ minHeight: '78.1vh' }}>
           <Switch>
             <Route path={process.env.PUBLIC_URL + '/'} exact component={Home} />
-            <Route path={process.env.PUBLIC_URL + '/github-repos'} exact component={GithubRepositories} />
             <Route path={process.env.PUBLIC_URL + '/cybortics-humanoid'} exact component={CyborticsHumanoids} />
             <Route path={process.env.PUBLIC_URL + '/ai'} exact component={Aihome} />
             <Route path={process.env.PUBLIC_URL + '/ai-dsm'} exact component={() => { Redirect('https://ai-dsm-gamer.netlify.app') }} />
-            {/* <Route path={process.env.PUBLIC_URL + '/quiz-science-hhw'} exact component={QuizLoad} /> */}
-            <Route path={process.env.PUBLIC_URL + '/Test'} exact component={Test} />
             <Route path="*" exact component={fourzerofour} />
           </Switch>
         </div>
