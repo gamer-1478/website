09a8ed64e537ee6ba06dee764295708f41b31c4a
Aayush Garg <74775129+gamer-1478@users.noreply.github.com>
2021-10-29 20:57:05 +0530

    Update Logger.js

--- src/Component/pages/Logger.js
+++ src/Component/pages/Logger.js
@@ -1,4 +1,5 @@
-import { React, useState, useEffect } from 'react'
+import React from 'react'
+//import { React, useState, useEffect } from 'react'
 //import firebase from '../../firebase';
 //import axios from 'axios'
 
